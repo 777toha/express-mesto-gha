776fe0f35ce63f1ae44b31240ae2d6d3d2972cd3
Anton Lyscov <anton.lyscov@yandex.ru>
2023-03-14 00:29:55 +0300

    Проверка серверных ответов и тесты

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
 app.use('/', userRouter);
 app.use('/', cardRouter);
-app.use((req, res) => {
+app.use('', (req, res) => {
   return res.status(NOTFOUND_CODE).send({ message: 'Запрашиваемый ресурс не найден' });
 })
 app.listen(PORT, () => {
